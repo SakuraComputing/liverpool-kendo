ece77b567965366227fe76453012b27b30f6f119
Ewan Bauld <ewan.bauld@gmail.com>
2018-02-22 22:22:00 +0000

    Add tests

--- src/tests/components/SideBar.test.js
+++ src/tests/components/SideBar.test.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import {Sidebar} from "../../components/SideBar";
+import Sidebar from '../../components/SideBar';
 
-test('should render the SideBar',() => {
-    const wrapper = shallow(<Sidebar />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
+describe('Side Bar', () => {
+    it('should render the side bar correctly', () => {
+        const wrapper = shallow(<Sidebar />);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    });
 });
-
-// test('should call start logout on button click',() => {
-//     const startLogout = jest.fn();
-//     const wrapper = shallow(<SideBar startLogout={startLogout} />);
-//     wrapper.find('button').simulate('click');
-//     expect(startLogout).toHaveBeenCalled();
-// });
-
