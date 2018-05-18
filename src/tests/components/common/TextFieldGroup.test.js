4d1df3a7c054f7e49eeee2eccd71ec7954f0fe77
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-18 18:29:55 +0100

    Add TextFieldComponent and test

---
+++ src/tests/components/common/TextFieldGroup.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TextFieldGroup from "../../../components/common/TextFieldGroup";
+
+
+describe('Text Field Group Component', () => {
+    it('should return a div component', () => {
+        wrapper = shallow(<TextFieldGroup />);
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('should render info',() => {
+       const info = jest.fn();
+       const wrapper = shallow(<TextFieldGroup info={info}/>);
+       expect(wrapper).toMatchSnapshot();
+    });
+    it('should render error', () => {
+        const error = jest.fn();
+        const wrapper = shallow(<TextFieldGroup error={error}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
