879f8265a4d9f4e31aa565d1b22abbef8ac92308
Ewan Bauld <ewan.bauld@gmail.com>
2018-07-29 18:13:05 +0100

    Add new home page and styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,11 +6,11 @@ export const LoginPage = ({startLogin}) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Liverpool Kendo Club</h1>
-            <p>Kendo</p>
-            <button className="button button--with-icon" onClick={() => startLogin('google')}><i className="icon-prepend fa fa-google"/>
+            <p className="box-layout__synopsis">"The way of the sword"</p>
+            <button className="button button--with-icon" onClick={() => startLogin('google')}><i className="icon-prepend fa fa-google button-icon"/>
                 Login with Google
             </button>
-            <button className="button button--with-icon" style={{"background":"#4267b2"}} onClick={() => startLogin('facebook')}><i className="icon-prepend fa fa-facebook-f"/>
+            <button className="button button--with-icon" style={{"background":"#4267b2"}} onClick={() => startLogin('facebook')}><i className="icon-prepend fa fa-facebook-f button-icon"/>
                 Login with Facebook
             </button>
         </div>
