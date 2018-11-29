bf1765ff889329b25ad985c875dde773c494a019
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-29 22:18:04 +0000

    Update events close meta tag on index html SEO

--- src/components/Events.js
+++ src/components/Events.js
@@ -14,9 +14,7 @@ export const Events = () => {
             <p></p>
             <h3>August TBC</h3>
             <p>
-
                 NB: There will also be a grading to Godan.
-
             </p>
             <h3>Kodokan International Seminar TBC</h3>
             <p>1. Sumi Sensei Hanshi Hachidan</p>
@@ -27,7 +25,6 @@ export const Events = () => {
             <h4>Northern Open - Glasgow 8th-9th December 2018</h4>
             <h4>Welsh Open 16th-17th February 2018</h4>
             <p></p>
-            <h4>Bowden and British Open 24th and 25th November 2019</h4>
             <br/>
 
         </div>
