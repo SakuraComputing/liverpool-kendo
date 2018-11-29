e39a3b9100dea0a759b6b5efde2e3b3927bf8f15
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-29 23:27:51 +0000

    Update events page.

--- src/components/Events.js
+++ src/components/Events.js
@@ -7,23 +7,27 @@ export const Events = () => {
 
             <h2>Gradings</h2>
             <p></p>
-            <p>Northern Grading</p>
-            <p>9th December - Glasgow</p>
+            <h3>United Kingdom - Glasgow</h3>
+            <p>9th December 2018</p>
+            <p>Grading to 4th Dan</p>
 
-            <h2>Summer Seminar</h2>
+            <h3>Belgium - Brussels</h3>
+            <p>3rd February 2019</p>
+            <p>Grading to 7th Dan</p>
+
+            <h2>Seminars</h2>
             <p></p>
-            <h3>August TBC</h3>
-            <p>
-                NB: There will also be a grading to Godan.
-            </p>
-            <h3>Kodokan International Seminar TBC</h3>
+            <h3>August 2019 London TBC</h3>
+            <p>Members of the all Japan kendo federation will be present last year 3 8th Dan attended</p>
+
+            <h3>Kodokan International Seminar 2019 TBC</h3>
             <p>1. Sumi Sensei Hanshi Hachidan</p>
             <p>2. Tashiro sensei Hachidan</p>
 
             <h2>Taikai</h2>
             <p></p>
             <h4>Northern Open - Glasgow 8th-9th December 2018</h4>
-            <h4>Welsh Open 16th-17th February 2018</h4>
+            <h4>Welsh Open 16th-17th February 2019</h4>
             <p></p>
             <br/>
 
