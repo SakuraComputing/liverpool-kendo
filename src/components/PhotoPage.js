73cec0d2dccf47b7a25f02022464877e0fb4042f
Ewan Bauld <ewan.bauld@gmail.com>
2018-11-29 23:13:43 +0000

    Update the photo section reduce the image sizes remove unwanted.

--- src/components/PhotoPage.js
+++ src/components/PhotoPage.js
@@ -5,12 +5,11 @@ import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-ho
 
 const photos = [
     { src: '/images/IMG_0966.jpg', width: 7, height: 5},
-    { src: '/images/IMG_3206.jpg', width: 4, height: 3 },
-    { src: '/images/kendokata.jpg', width: 1, height: 1 },
-    { src: '/images/tsuki.jpeg', width: 5, height: 4 },
-    { src: '/images/do.jpg', width: 4, height: 4 },
+    { src: '/images/keiko2.png', width: 5, height: 3 },
+    { src: '/images/do.jpg', width: 4, height: 3 },
     { src: '/images/alljapan.jpg', width: 5, height: 4 },
-    { src: '/images/teramoto.jpg', width: 6, height: 6 },
+    { src: '/images/teramoto.jpg', width: 6, height: 5 },
+    { src: '/images/bowden.jpg', width: 6, height: 5 },
 ];
 
 const SortablePhoto = SortableElement(Photo);
