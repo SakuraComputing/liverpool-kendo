76c187428c2a24649d2d47f9d65e6937731d6c6f
Ewan Bauld <ewan.bauld@gmail.com>
2019-02-25 23:41:39 +0000

    Update events, upgrade node-sass to work woth node 10.15.0

--- src/components/Events.js
+++ src/components/Events.js
@@ -6,29 +6,21 @@ export const Events = () => {
             <h1>Events coming up</h1>
 
             <h2>Gradings</h2>
-            <p></p>
-            <h3>United Kingdom - Glasgow</h3>
-            <p>9th December 2018</p>
-            <p>Grading to 4th Dan</p>
-
-            <h3>Belgium - Brussels</h3>
-            <p>3rd February 2019</p>
-            <p>Grading to 7th Dan</p>
+            <p>Northern Seminar and Grading March - Glasgow</p>
 
             <h2>Seminars</h2>
+            <p>Oxford 16th-17th March - Tashiro Sensei 8th Da, Hiyama sensei 7th Dan</p>
+            <p>Spring Seminar 30th - 31st March 2019 and Kashi No Ki (Nottingham) - Ninomiya sensei 7th Dan</p>
             <p></p>
             <h3>August 2019 London TBC</h3>
-            <p>Members of the all Japan kendo federation will be present last year 3 8th Dan attended</p>
+            <p>Members of the all Japan kendo federation will be present last year 3 8th Dan attended.</p>
 
             <h3>Kodokan International Seminar 2019 TBC</h3>
             <p>1. Sumi Sensei Hanshi Hachidan</p>
             <p>2. Tashiro sensei Hachidan</p>
 
             <h2>Taikai</h2>
-            <p></p>
-            <h4>Northern Open - Glasgow 8th-9th December 2018</h4>
-            <h4>Welsh Open 16th-17th February 2019</h4>
-            <p></p>
+            <p>London Cup 3rd-4th April</p>
             <br/>
 
         </div>
