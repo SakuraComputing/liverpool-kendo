b60246df14d9c892c99e29156f4cc03d84cf3a1d
Ewan Bauld <ewan.bauld@gmail.com>
2019-07-31 22:21:50 +0100

    Add google verify

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,12 +5,7 @@ export const Events = () => {
         <div className="content-container">
             <h1>Events coming up</h1>
 
-            <h2>Gradings</h2>
-            <p>Northern Seminar and Grading March - Glasgow</p>
-
             <h2>Seminars</h2>
-            <p>Oxford 16th-17th March - Tashiro Sensei 8th Da, Hiyama sensei 7th Dan</p>
-            <p>Spring Seminar 30th - 31st March 2019 and Kashi No Ki (Nottingham) - Ninomiya sensei 7th Dan</p>
             <p></p>
             <h3>August 2019 London TBC</h3>
             <p>Members of the all Japan kendo federation will be present last year 3 8th Dan attended.</p>
