865d21b8cc0fa5ee89e349faf99b278f732e4641
Ewan Bauld <ewan.bauld@gmail.com>
2021-02-28 00:48:30 +0000

    Update styling for banner on desktop and mobile

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Covid = () => (
     <div className="covid-content" >
-        <h1>Coronavirus (COVID-19)</h1>
-        <div className="covid-desc">Due to COVID the dojo will obviously remain closed until the government restrictions are lifted</div>
-        <a 
-            href="https://www.gov.uk/guidance/coronavirus-covid-19-grassroots-sports-guidance-for-safe-provision-including-team-sport-contact-combat-sport-and-organised-sport-events"
-            className="covid-gov-link"
-        >
-            Government Contact Combat Sport Guidelines
-        </a>
+        <div className="covid-title" >Coronavirus (COVID-19)</div>
+        <div className="covid-desc">Due to COVID the dojo will remain closed until the government restrictions are lifted. (See Link below)</div>
+        <div className="covid-footer" >
+            <a 
+                href="https://www.gov.uk/guidance/coronavirus-covid-19-grassroots-sports-guidance-for-safe-provision-including-team-sport-contact-combat-sport-and-organised-sport-events"
+                className="covid-gov-link"
+            >
+                Government Contact Combat Sport Guidelines
+            </a>
+        </div>
     </div>
 );
 export default Covid;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -9,16 +9,19 @@ export const LoginPage = ({startLogin}) => (
         <div className="box-layout">
             <div className="box-layout__box">
                 <h1 className="box-layout__title">Liverpool Kendo Club</h1>
-                <Covid />
                 <p className="box-layout__synopsis">"The way of the sword"</p>
-                <button className="button button--with-icon" onClick={() => startLogin('google')}><i className="icon-prepend fa fa-google button-icon"/>
-                    Login with Google
-                </button>
-                <button className="button button--with-icon" style={{"background":"#4267b2"}} onClick={() => startLogin('facebook')}><i className="icon-prepend fa fa-facebook-f button-icon"/>
-                    Login with Facebook
-                </button>
+                <Covid />
             </div>
         </div>
+        <div className="footer-buttons" >
+            <button className="button button--with-icon" onClick={() => startLogin('google')}><i className="icon-prepend fa fa-google button-icon"/>
+                Login with Google
+            </button>
+            <button className="button button--with-icon" style={{"background":"#4267b2"}} onClick={() => startLogin('facebook')}><i className="icon-prepend fa fa-facebook-f button-icon"/>
+                Login with Facebook
+            </button>
+        </div>
+
         <a className="footer-text" href="http://www.britishkendoassociation.com" target="_Blank">Member of the British Kendo Association<img src="/images/bka_logo3.png" alt="BKA Logo" className="bka-logo"/></a>
     </div>
 );
