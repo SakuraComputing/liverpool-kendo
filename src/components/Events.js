27ee206388111ff50aa97176ac2fbdaced19755e
Ewan Bauld <ewan.bauld@gmail.com>
2021-03-02 10:11:13 +0000

    Update videos and tidy up

--- src/components/Events.js
+++ src/components/Events.js
@@ -6,13 +6,7 @@ export const Events = () => {
             <h1>Events coming up</h1>
 
             <h2>Seminars</h2>
-            <p></p>
-            <h3>August 2019 London TBC</h3>
-            <p>Members of the all Japan kendo federation will be present last year 3 8th Dan attended.</p>
-
-            <h2>Taikai</h2>
-            <p>London Cup 3rd-4th April</p>
-            <br/>
+            <p>None until the government lift the COVID restrictions</p>
 
         </div>
     )
